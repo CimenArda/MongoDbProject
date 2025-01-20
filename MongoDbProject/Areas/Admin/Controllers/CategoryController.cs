f3c7d1e9f0f5422d7178cbda38f506ed672d5b4c
ArdaCimen <cimenarda.yaz@gmail.com>
2025-01-20 12:36:18 +0300

    AdminLayout,Kategori İşlemleri

---
+++ MongoDbProject/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDbProject.Dtos.CategoryDtos;
+using MongoDbProject.Services.CategoryServices;
+
+namespace MongoDbProject.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/Category/")]
+	public class CategoryController : Controller
+	{
+
+		private readonly ICategoryService _categoryService;
+
+		public CategoryController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		[Route("Index")]
+		public async Task<IActionResult> Index()
+		{
+			var value = await _categoryService.GetAllCategoryAsync();
+			return View(value);
+		}
+
+		[HttpGet]
+		[Route("CreateCategory")]
+		public async Task<IActionResult> CreateCategory()
+		{
+			return View();
+
+		}
+
+
+		[HttpPost]
+		[Route("CreateCategory")]
+		public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+		{
+			_categoryService.CreateCategoryAsync(createCategoryDto);
+			return RedirectToAction("Index");	
+
+		}
+
+		[Route("DeleteCategory/{id}")]
+		public async Task<IActionResult> DeleteCategory(string id)
+		{
+			_categoryService.DeleteCategoryAsync(id);
+           return RedirectToAction("Index");
+
+
+		}
+
+		[Route("UpdateCategory/{id}")]
+		public async Task<IActionResult> UpdateCategory(string id)
+		{
+			var category = await _categoryService.GetByIdCategoryAsync(id);
+			return View(category);
+		}
+
+		[Route("UpdateCategory/{id}")]
+		[HttpPost]
+		public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+		{
+			await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+			return RedirectToAction("Index");
+
+		}
+
+
+
+
+
+	}
+}
---
+++ MongoDbProject/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MongoDbProject.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+	public class _AdminLayoutScriptComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
