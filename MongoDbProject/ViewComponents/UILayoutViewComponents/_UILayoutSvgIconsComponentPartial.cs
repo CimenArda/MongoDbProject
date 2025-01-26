bec358b3cc3edce64e5ca34ce46fe13b677c9197
ArdaCimen <cimenarda.yaz@gmail.com>
2025-01-27 01:10:50 +0300

    UILayout View Component

---
+++ MongoDbProject/ViewComponents/UILayoutViewComponents/_UILayoutSvgIconsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MongoDbProject.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutSvgIconsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
